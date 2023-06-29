b91a8e7f75471ecb7920551018c2c708f5a7864f
Lisandro Rodriguez <licharp41@gmail.com>
2023-06-29 17:13:44 -0300

    clase 4 lista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Lista-RodriguezP-Lisandro';
+  numero = 0;
+  hayError = true;
+  alumnos = ['Lisandro', 'Manuel', 'Sofia', 'Belen','Pedro','Maria Jose','Hector'];
+
+  myFontSize = '20px';
 }
